8d9014a12b3b3be4e1bccc43171d3db50150566e
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-12 21:52:52 +0200

    Add push method on CodersArray class

--- public/CodersArray/CodersArray.js
+++ public/CodersArray/CodersArray.js
@@ -4,6 +4,20 @@ class CodersArray {
   constructor(...elements) {
     this.elements = elements;
   }
+
+  length() {
+    let numberElements = 0;
+    for (let i = 0; i < this.elements.length; i++) {
+      numberElements++;
+    }
+
+    return numberElements;
+  }
+
+  push(addedValues) {
+    const length = this.length();
+    this.elements[length] = addedValues;
+  }
 }
 
 export default CodersArray;
--- public/index.js
+++ public/index.js
@@ -2,4 +2,8 @@ import CodersArray from "./CodersArray/CodersArray.js";
 
 const ejemplo = new CodersArray(3, 8, 10, 2);
 
+console.log(ejemplo.length());
+
+ejemplo.push(1);
+
 console.log(ejemplo);
