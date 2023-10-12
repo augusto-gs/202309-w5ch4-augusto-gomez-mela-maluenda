a2acca05fbf93de2359728cbe2ccb128f194e520
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-13 00:09:38 +0200

    Add map method to CodersArray class

--- public/index.js
+++ public/index.js
@@ -2,8 +2,12 @@ import CodersArray from "./CodersArray/CodersArray.js";
 
 const ejemplo = new CodersArray(3, 8, 10, 2);
 
-console.log(ejemplo.length());
+// Console.log(ejemplo.length());
 
-ejemplo.push();
+// ejemplo.push();
 
-console.log(ejemplo);
+// console.log(ejemplo);
+
+console.log(ejemplo.map((number) => number * 5));
+
+console.log(2);
